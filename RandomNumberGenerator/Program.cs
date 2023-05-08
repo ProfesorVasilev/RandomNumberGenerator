a9a9bd9cd0dd0764eeb1baf6e0cfef9fed84cec3
ProfesorVasilev <vladigotaniq@gmail.com>
2023-05-08 12:19:47 +0300

    Add project files.

---
+++ RandomNumberGenerator/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Whats your min value?");
+int min = int.Parse(Console.ReadLine());
+Console.WriteLine("whats your max value?");
+int max = int.Parse(Console.ReadLine());
+Random randomGenerator= new Random();               //random number generator
+int randomNumber = randomGenerator.Next(min, max); // ^
+Console.WriteLine("Guess the number!");
+while (true)
+{
+    int guess = int.Parse(Console.ReadLine());
+    if (guess == randomNumber)
+    {
+        Console.WriteLine("Correct");
+        break;
+    } 
+    else if (guess > randomNumber)
+    {
+        Console.WriteLine("The number is smaller, try again");
+    }
+    else if (guess < randomNumber)
+    {
+        Console.WriteLine("The number is bigger, try again");
+    }
+        
+}
